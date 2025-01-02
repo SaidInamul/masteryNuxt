f36041e98834d56d1d6e3adf3d7c938629ab7c9a
SaidHafizzan <100667330+SaidInamul@users.noreply.github.com>
2025-01-03 00:15:41 +0800

    styling the app with bulma framework

--- app.vue
+++ app.vue
@@ -1,43 +1,62 @@
-<script>
-import { defineNuxtComponent } from "#app"; //
+<script setup>
+// import { defineNuxtComponent } from "#app"; //
 
-export default defineNuxtComponent({
-    data: () => ({
-        todoList: [],
-    }),
-    computed: {
-        completedItems() {
-            // Filter the todoList as an item that is completed
-            return this.todoList.filter((item) => item.completed);
-        },
-        remainingItems() {
-            // Filter the todoList as an item that is completed
-            return this.todoList.filter((item) => !item.completed);
-        },
-    },
-    methods: {
-        fetchTodoData() {
-            // fetch return a promise
-            fetch("https://jsonplaceholder.typicode.com/todos/")
-                // then(onFulfilled, onRejected) to handle the result of the promise
-                .then((response) => response.json()) // parse the json string into js object
-                .then(
-                    (json) => (this.todoList = json), //after the parsed it becomes a resolve promise, it can be named anything ex. json, data, result dll
-                )
-                .catch((error) => console.log("error : " + error));
-        },
-    },
+// Composition API - the reactive data wraps the value inside an object
+// In tempalte section, vue automatically unwraps refs when they are accessed making it more intuitive(Easer for human)
+import { ref, computed } from "vue";
+
+const todoList = ref([]);
+
+const completedItems = computed(() => {
+    return todoList.value.filter((item) => item.completed);
 });
+
+const remainingItems = computed(() => {
+    return todoList.value.filter((item) => !item.completed);
+});
+
+const fetchTodoData = () => {
+    fetch("https://jsonplaceholder.typicode.com/todos/")
+        // then(onFulfilled, onRejected) to handle the result of the promise
+        .then((response) => response.json()) // parse the json string into js object
+        .then(
+            (json) => (todoList.value = json), //after the parsed it becomes a resolve promise, it can be named anything ex. json, data, result dll
+        )
+        .catch((error) => console.log("error : " + error));
+};
+
+// Option API
+// export default defineNuxtComponent({
+//     data: () => ({
+//         todoList: [],
+//     }),
+//     computed: {
+//         completedItems() {
+//             // Filter the todoList as an item that is completed
+//             return this.todoList.filter((item) => item.completed);
+//         },
+//         remainingItems() {
+//             // Filter the todoList as an item that is completed
+//             return this.todoList.filter((item) => !item.completed);
+//         },
+//     },
+//     methods: {
+//         fetchTodoData() {
+//             // fetch return a promise
+//             fetch("https://jsonplaceholder.typicode.com/todos/")
+//                 // then(onFulfilled, onRejected) to handle the result of the promise
+//                 .then((response) => response.json()) // parse the json string into js object
+//                 .then(
+//                     (json) => (this.todoList = json), //after the parsed it becomes a resolve promise, it can be named anything ex. json, data, result dll
+//                 )
+//                 .catch((error) => console.log("error : " + error));
+//         },
+//     },
+// });
 </script>
 
 <template>
-    <div>
-        <img
-            src="/todo.jpg"
-            alt="Todo photo by dlxmedia.hu"
-            height="307px"
-            width="520px"
-        />
+    <div class="section">
         <p>
             Photo by
             <a
@@ -52,13 +71,13 @@ export default defineNuxtComponent({
                 Unsplash
             </a>
         </p>
-        <h1>Hello world nuxt js 3</h1>
-        <button @click="fetchTodoData">Fetch data</button>
+        <h1 class="title">Hello world nuxt js 3</h1>
+        <button class="button" @click="fetchTodoData">Fetch data</button>
         <p>
             {{ completedItems.length }} items completed |
             {{ remainingItems.length }} remaining items
         </p>
-        <ul>
+        <ul class="list">
             <li v-for="todo in todoList" :key="`todo-id-${todo.id}`">
                 <input type="checkbox" :checked="todo.completed" />
                 {{ todo.title }}
@@ -66,3 +85,21 @@ export default defineNuxtComponent({
         </ul>
     </div>
 </template>
+
+<style lang="scss">
+@import "./node_modules/bulma/bulma.scss";
+@import "./assets/styles/main.scss";
+
+:root {
+    --text-color: #{$textColor};
+}
+.title {
+    color: var(--text-color);
+}
+
+.list {
+    color: var(--text-color);
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+}
+</style>
